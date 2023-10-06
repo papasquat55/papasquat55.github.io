221a7d8ae9b1f9b48934728fe038162ff0b88050
papasquat55 <papasquat55@gmail.com>
2023-10-06 12:42:10 -0700

    Batch update

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":959},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-services-offered","displayName":"Services Offered","link":{"linkType":"LinkTypePage","href":"#!page-services-offered"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-our-team","displayName":"Our Team","link":{"linkType":"LinkTypePage","href":"#!page-our-team"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+/* slider buttons */
+$(function() {
+    try {
+        var slider = $('.slider .unslider').children().get(0);
+        var unslider = $(slider).data('unslider');
+        $('.sub-nav').click(function() {
+            unslider.next()
+            $('.sub-nav-active').removeClass('sub-nav-active');
+            $(this).addClass('sub-nav-active');
+        });
+    }
+    catch(e) {
+    }
+})
